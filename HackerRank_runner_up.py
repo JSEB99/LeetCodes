b8c9583369b10f548277619e3bf362aed45d4b30
Sebastian Mora <sebastian.mt99@gmail.com>
2023-07-24 20:01:04 -0500

    adding docstrings

--- HackerRank_runner_up.py
+++ HackerRank_runner_up.py
@@ -3,6 +3,14 @@ if __name__ == '__main__':
     arr = map(int, input().split())
 
     def max_number(arr):
+        '''
+        description: max number of a list (between -100 to 100)
+
+        args = list of numbers
+
+        output = return the max number and the list of numbers
+                highest_score,lista_temp
+        '''
         highest_score = -100
         lista_temp = []
         for number in arr:
@@ -12,6 +20,13 @@ if __name__ == '__main__':
         return highest_score,lista_temp
     
     def runner_up_score(highest_score,number_list):
+        '''
+        description = runner_up number of a list
+
+        args = highest_score,number_list
+
+        output = runner_up number
+        '''
         minimun_number = -100
         for number in number_list:
             if (number>minimun_number) and (number<highest_score):
@@ -20,4 +35,3 @@ if __name__ == '__main__':
     
     numero_mayor,lista_numeros = max_number(arr)
     print(runner_up_score(numero_mayor,lista_numeros))
-    
