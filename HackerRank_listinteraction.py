9853b1f441ff7519d67c343c3b4c0e4fe8756702
Sebastian Mora <sebastian.mt99@gmail.com>
2023-07-27 16:10:06 -0500

    solution of the commands on a list

--- HackerRank_listinteraction.py
+++ HackerRank_listinteraction.py
@@ -1,5 +1,3 @@
-
-
 if __name__ == '__main__':
     N = int(input())
     
@@ -11,5 +9,18 @@ if __name__ == '__main__':
     for command in commands:
         if command[0]=='append':
             output.append(int(command[1]))
-    
-    print(output)
+        elif command[0]=='insert':
+            output.insert(int(command[1]),int(command[2]))
+        elif command[0]=='print':
+            print(output)
+        elif command[0]=='remove':
+            output.remove(int(command[1]))
+        elif command[0]=='sort':
+            output.sort()
+        elif command[0]=='pop':
+            if len(command)==1:
+                output.pop()
+            else:
+                output.pop(command[1])
+        elif command[0]=='reverse':
+            output.reverse()
